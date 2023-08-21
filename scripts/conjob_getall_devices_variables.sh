2bec3c9b1232682e71eb7556575fba6f4018c517
vincent agi <vincent@inviseo.fr>
2023-08-21 11:35:49 +0200

    add crontab script to auto get data

--- scripts/conjob_getall_devices_variables.sh
+++ scripts/conjob_getall_devices_variables.sh
@@ -1,1 +1,17 @@
-grep 'root python /home/pyscada/pyscada-lite manage.py get_all_devices_variables' /etc/crontab || echo '* * * * * root python /home/pyscada/pyscada-lite manage.py get_all_devices_variables' >> /etc/crontab
+#!/bin/sh 
+# check to insure the entry does not already exist 
+exists=0
+crontab -l | grep "python3 /home/${USER}/pyscada-lite/pyscada-lite-back/manage.py get_all_variables_devices"  && exists=1 || exists=0
+if [ $exists -eq 1 ]
+then
+echo "cronjob exist"
+exit;
+fi
+# copy the contents of the current crontab to a temporary file 
+crontab -l >/tmp/crontab.tmp 
+# add the new entry to the end of the temporary file 
+echo "* * * * * python3 /home/${USER}/pyscada-lite/pyscada-lite-back/manage.py get_all_variables_devices" >> /tmp/crontab.tmp 
+#update crontab with the contents of the temporary file 
+crontab /tmp/crontab.tmp 
+# delete the temporary file 
+rm /tmp/crontab.tmp 
