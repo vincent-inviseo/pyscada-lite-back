b3c80139505b3a4c32471104bce06b3908631a66
vicnent agi <vicnent@inviseo.fr>
2023-08-29 08:13:05 +0200

    fix csrf cookie validation

---
+++ config/csrf_middleware.py
@@ -0,0 +1,7 @@
+from django.utils.deprecation import MiddlewareMixin
+
+
+
+class BypassCSRFMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        setattr(request, "_dont_enforce_csrf_checks", True)
